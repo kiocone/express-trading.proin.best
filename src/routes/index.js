b7f6583fbe47695ad84eefb6e5ef470c7f26725e
Kiocone <kiocone@gmail.com>
2021-06-11 11:30:30 -0500

    test git

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('<h1>Proin.best - Login </h1><br><a href="/panel" style=text: "center">Ir al panel</a>')
+    res.send('<h1>Proin.best - Bienvenido </h1><br><a href="/panel" style=text: "center">Ir al panel</a>')
 });
 
 module.exports = router;
